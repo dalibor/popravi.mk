c63299ae431354fd4fd67db0a95d66f5ac4bd41c
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2010-08-02 23:25:45 +0200

    List latest problems and tweets on welcome page
    * List latest problems on welcome page
    * List latest tweets from PopraviMK twitter user on welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,6 @@
 class WelcomeController < ApplicationController
+
+  def index
+    @problems = Problem.find :all, :order => "id DESC", :limit => 5, :include => [:category, :municipality]
+  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,42 @@
+var DateHelper = {
+  // Takes the format of "Jan 15, 2007 15:45:00 GMT" and converts it to a relative time
+  // Ruby strftime: %b %d, %Y %H:%M:%S GMT
+  time_ago_in_words_with_parsing: function(from) {
+    var date = new Date; 
+    date.setTime(Date.parse(from));
+    return this.time_ago_in_words(date);
+  },
+  
+  time_ago_in_words: function(from) {
+    return this.distance_of_time_in_words(new Date, from);
+  },
+ 
+  distance_of_time_in_words: function(to, from) {
+    var distance_in_seconds = ((to - from) / 1000);
+    var distance_in_minutes = Math.floor(distance_in_seconds / 60);
+ 
+    if (distance_in_minutes == 0) { return 'пред помалку од минута'; }
+    if (distance_in_minutes == 1) { return 'пред минута'; }
+    if (distance_in_minutes < 45) { return 'пред ' + distance_in_minutes + ' минути'; }
+    if (distance_in_minutes < 90) { return 'пред 1 час'; }
+    if (distance_in_minutes < 1440) { return 'пред ' + Math.floor(distance_in_minutes / 60) + ' часа'; }
+    if (distance_in_minutes < 2880) { return 'пред 1 ден'; }
+    if (distance_in_minutes < 43200) { return 'пред ' + Math.floor(distance_in_minutes / 1440) + ' дена'; }
+    if (distance_in_minutes < 86400) { return 'пред 1 месец'; }
+    if (distance_in_minutes < 525960) { return 'пред ' + Math.floor(distance_in_minutes / 43200) + ' месеци'; }
+    if (distance_in_minutes < 1051199) { return 'пред 1 година'; }
+
+    return 'пред ' + (distance_in_minutes / 525960).floor() + ' години';
+  }
+};
+
+jQuery.fn.autolink = function () {
+  return this.each( function(){
+    var r = /((http|https|ftp):\/\/[\w?=&.\/-;#~%-]+(?![\w\s?&.\/;#~%"=-]*>))/g;
+    $(this).html( $(this).html().replace(r, '<a href="$1">$1</a> ') );
+  });
+}
+
 var problems_edit = problems_create = problems_update = problems_new = {
   run: function () {
     var map;
@@ -151,6 +190,30 @@ var problems_show = {
     }
 
     initialize();
+  }
+}
+
+var welcome_index = {
+  run: function () {
+    var user_timeline = function(username, count, callback){
+      requestURL = "http://twitter.com/statuses/user_timeline/" + username + ".json?callback=?&count=" + count;
+      $.getJSON(requestURL, callback);
+    };
+    user_timeline("PopraviMK", 5, function (json, status) {
+      var tweets = $("#tweets");
+      $.each(json, function(i) {
+        tweets.append(
+          $("<p/>").append(
+            $('<a/>').attr({href: "http://twitter.com/" + this.user.screen_name}).append(
+              $('<img/>').attr({"src": this.user.profile_image_url})
+            ),
+            $('<div/>').attr({"class": "t_user"}).text(this.user.screen_name),
+            $('<div/>').attr({"class": "t_text"}).text(this.text),
+            $('<div/>').attr({"class": "t_date"}).text(DateHelper.time_ago_in_words_with_parsing(this.created_at))
+          ).autolink()
+        )
+      });
+    });
   }
 }
 
