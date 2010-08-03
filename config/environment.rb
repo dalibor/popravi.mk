d9daf2064b06d90433515a8f68600a52dd9b490f
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2010-08-03 23:44:55 +0200

    Views localization
    * Changed devise version 1.0.8 for Rails 2.3.8
    * Removed old devise templates, generated new and changed them to use formtastic
    * Views localization

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'warden', :version => '>=0.10.3', :source => 'http://gemcutter.org'
-  config.gem 'devise', :version => '>=1.0.6', :source => 'http://gemcutter.org'
+  config.gem 'devise', :version => '=1.0.8', :source => 'http://gemcutter.org'
   config.gem "compass", :version => ">= 0.10.2"
   config.gem 'compass-960-plugin', :lib => false
   config.gem 'haml'
