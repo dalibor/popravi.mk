ebbdf3c8653292f19572edafa3109d22ffb60730
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2010-08-06 00:01:57 +0200

    Added social share icons, added helpers for for layout head

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,22 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def javascript(*files)
+    content_for(:head) { javascript_include_tag(*files) }
+  end
+  
+  def stylesheet(*files)
+    content_for(:head) { stylesheet_link_tag(*files) }
+  end
+  
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+  
+  def keywords(page_keywords)
+    content_for(:keywords) { page_keywords }
+  end
+  
+  def description(page_description)
+    content_for(:description) { page_description }
+  end
 end
