0fc4288313879edc3ee0d45715d629b8b155c200
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2010-09-11 01:17:47 +0200

    Added last 3 blogs to front page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,7 @@ class WelcomeController < ApplicationController
 
   def index
     @problems = Problem.with_photo
+    @now = Time.now
+    @posts = Post.published.for_month(@now.year, @now.month).paginate :per_page => 3, :page => params[:page]
   end
 end
