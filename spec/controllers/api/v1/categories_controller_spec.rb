d8bb3aa00c8ce77a56cfdd996030b13756f68c5d
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2010-10-16 18:34:26 +0200

    Added specs for API:V1 categories & municipalities

---
+++ spec/controllers/api/v1/categories_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Api::V1::CategoriesController do
+  context "API V1 categories" do
+    it "lists empty json when no categories" do
+      get :index, :format => 'json'
+      response.body.should == '[]'
+    end
+
+    it "lists hash with categories" do
+      Factory.create(:category1, :id => 1)
+      Factory.create(:category2, :id => 2)
+
+      get :index, :format => 'json'
+      response.body.should == '[{"name":"Category 1","id":1},{"name":"Category 2","id":2}]'
+    end
+
+    it "lists hash with categories ordered by position" do
+      category1 = Factory.create(:category1, :id => 1)
+      category2 = Factory.create(:category2, :id => 2)
+      category2.move_higher
+
+      get :index, :format => 'json'
+      response.body.should == '[{"name":"Category 2","id":2},{"name":"Category 1","id":1}]'
+    end
+  end
+end
---
+++ spec/controllers/api/v1/municipalities_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Api::V1::MunicipalitiesController do
+  context "API V1 municipalities" do
+    it "lists empty json when no municipalities" do
+      get :index, :format => 'json'
+      response.body.should == '[]'
+    end
+
+    it "lists hash with categories" do
+      Factory.create(:municipality1, :id => 1)
+      Factory.create(:municipality2, :id => 2)
+
+      get :index, :format => 'json'
+      response.body.should == '[{"name":"Municipality 1","id":1},{"name":"Municipality 2","id":2}]'
+    end
+
+    it "lists json with municipalities ordered by name" do
+      Factory.create(:municipality2, :id => 1)
+      Factory.create(:municipality1, :id => 2)
+
+      get :index, :format => 'json'
+      response.body.should == '[{"name":"Municipality 1","id":2},{"name":"Municipality 2","id":1}]'
+    end
+  end
+end
