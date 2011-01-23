e6cc397b6e6c44420f9f0fb7da1222691a0c43ad
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-01-23 01:36:45 +0100

    Changed staging capistrano deployment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 set :application, "staging.popravi.mk"
-set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_to, "/home/#{user}/www/#{application}"
 set :rails_env, "development"
 set :domain, "#{user}@#{application}"
 
