d81fe40b5468fbce8d2b027da4aa607120fe61c7
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-04-09 12:31:56 +0200

    Fixed bug in migration when setting up municipality slug

--- db/migrate/20110226212030_add_slug_to_municipalities.rb
+++ db/migrate/20110226212030_add_slug_to_municipalities.rb
@@ -2,6 +2,7 @@ class AddSlugToMunicipalities < ActiveRecord::Migration
   def self.up
     add_column :municipalities, :slug, :string
 
+    Municipality.reset_column_information
     Municipality.all.each do |municipality|
       municipality.save
     end
