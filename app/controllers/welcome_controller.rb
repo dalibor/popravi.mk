58bcb2e1de58fd4f1d5bab672ff4ffb9260f9bee
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-12-07 09:26:32 +0100

    Fixed blog post and translation

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,15 @@
 class WelcomeController < ApplicationController
-  
+
   layout "welcome"
-  
+
   def index
     @problems = Problem.ordered.includes([:category, :municipality]).limit(5)
     @municipalities = Municipality.order('problems_count DESC').limit(5)
-    @post = Post.published.ordered.first
+    @post = Post.from_admins.published.ordered.first
     @problem = Problem.with_status('solved').order('solved_at DESC').first
   end
-  
+
   def android
   end
-  
+
 end
