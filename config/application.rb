8c8c8c4959f4e9caf889a50927d0f59a001f22c6
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2012-12-22 23:17:42 +0100

    Precompile admin assets with assets pipeline

--- config/application.rb
+++ config/application.rb
@@ -74,6 +74,11 @@ module PopraviMk
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.precompile += ['admin.js', 'admin.css']
+
+    # don't access the DB or load models when precompiling assets.
+    config.assets.initialize_on_precompile = false
+
     config.generators do |g|
       g.template_engine :haml
       g.test_framework :rspec, :fixture => false
